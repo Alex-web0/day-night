abb1bdc496f1508c1cdef926ffe5171cf0d385e0
Alex-web0 <89301122+Alex-web0@users.noreply.github.com>
2021-09-26 13:23:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function makeDark(){
+    document.querySelector('#text').classList.toggle("light-text")
+    document.querySelector('#cont').classList.toggle("light-text")
+    document.querySelector('body').classList.toggle("dark-back")
+    document.querySelector('body').classList.toggle("light-text")
+    document.querySelector('button').click.classList.toggle("light-text")
+}
